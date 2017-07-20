367619bc13802733bbd82836b41dfa72577c1124
sacha markides <sacha.markides@orange.fr>
2017-07-20 11:13:55 +0200

    Ajout TP4

--- src/main/java/imipoecjava2017/PoecJavaApplication.java
+++ src/main/java/imipoecjava2017/PoecJavaApplication.java
@@ -38,10 +38,48 @@ public class PoecJavaApplication {
 		System.out.println("Nombre de pomme de Aladin :");
 		aladin = lire.nextInt();
 		
-		lire.close(); // JAVA -> Ferme le scanner
+		pannierCommun = jasmine + aladin;
+		
+		//lire.close();  JAVA -> Ferme le scanner
 		
 		// Algo : Ecrire
 		System.out.println(pannierCommun);
+		
+		// Test Conditionnelle :
+		// si alors sinon si sinon en java => if(condition) else if(condition) else 
+		// test égalité en java => "=="
+		// NON(condition) => !(condition)
+		// ET => &&
+		// OU => ||
+		
+		int chiffre1 ;
+		int chiffre2 ;
+		int res = 0;
+		
+		System.out.println("Chiffre 1 :");
+		chiffre1 = lire.nextInt();
+		System.out.println("Chiffre 2 :");
+		chiffre2 = lire.nextInt();
+		
+		if (chiffre1==chiffre2){
+			res = chiffre1 + chiffre2;			
+		}else if((chiffre1 < 0) && (chiffre2 > 0)){
+			res = chiffre1 * chiffre2;
+		}else if( ( ((chiffre1 > 0) && (chiffre2 > 0))          ||
+				    ((chiffre1 < 0) && (chiffre2 < 0))     )    && 
+				  ( ((chiffre1 > 10) || (chiffre1 < -10))       || 
+				    ((chiffre2 > 10) || (chiffre2 < -10))  )       ){
+			res = chiffre1 / chiffre2;
+		} 
+		
+		if(res!=0){
+			System.out.println(res);
+		}else{
+			System.out.println("J'ai fait aucun test");
+		}
+	
+		
+		
 	}
 
 }
