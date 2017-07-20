0353619852448d29ad57e633454d89636678efe9
sacha markides <sacha.markides@orange.fr>
2017-07-20 09:39:21 +0200

    Ajout TP2

--- src/main/java/imipoecjava2017/PoecJavaApplication.java
+++ src/main/java/imipoecjava2017/PoecJavaApplication.java
@@ -8,13 +8,28 @@ package imipoecjava2017;
  *
  */
 public class PoecJavaApplication {
+	
+	// ";" instruction de fin de ligne
+	
+	// Algo : déclaration variable entière
+	// ENTIER : momVar => int nomVar
+	
+	// Algo : En java static est noté car utilisé dans une fonction
+	// static int jasmine = 7;
+	// static int aladin = 5; 
+	// static int pannierCommun = jasmine + aladin;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			System.out.println("Hello World");
+		// Algo : Alternative au static, déclaration drirectement des variables
+		//à l'intérieur de la focntion
+		int jasmine = 7;
+		int aladin = 5;
+		int pannierCommun = jasmine + aladin;
+		// Algo : Ecrire
+		System.out.println(pannierCommun);
 	}
 
 }
