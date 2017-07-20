1bc89eadf7977f68853329cb3db054a6e9215563
sacha markides <sacha.markides@orange.fr>
2017-07-20 09:14:13 +0200

    Init project with qualities

---
+++ src/main/java/imipoecjava2017/PoecJavaApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package imipoecjava2017;
+
+/**
+ * @author Sacha
+ *
+ */
+public class PoecJavaApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Hello World");
+	}
+
+}
